32beef11b11d7ca4ad0148b6199ff4e1bec0c803
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-02-18 15:42:19 +0100

    create next app + initial styling

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript} from "next/document"; 
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html>
+            <Head> 
+                <link 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" 
+                    rel="stylesheet" 
+                />
+                <link rel="stylesheet" href="public/css/globals.css" />
+            </Head>
+            <body>
+                <div className="container">
+                    <Main />
+                </div>
+                <NextScript />
+            </body>
+        </Html>
+    );
 }

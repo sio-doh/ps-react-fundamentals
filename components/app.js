635e1b632bdda053e46af5cbf0f2079c0695e0eb
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-02-20 12:47:17 +0100

    Hooks,Props and State

--- components/app.js
+++ components/app.js
@@ -1,7 +1,18 @@
 import Banner from "./banner";
+import HouseList from "./houseList";
 
-const App = () => {
-    return <Banner />;
+const App = () => { 
+    const jsx = <div>Hi</div>;
+
+    return (
+        <>
+            {jsx}
+            <Banner>
+                <div>Providing houses all over the world</div>
+            </Banner>
+            <HouseList />
+        </>
+    );
 };
 
 export default App;
